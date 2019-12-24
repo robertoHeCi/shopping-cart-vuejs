a102031ab167223f67e3e1d094edbfd1b8e4d077
Roberto Hernández <robertohc@gmail.com>
2019-12-24 15:40:27 +0100

    Create pages

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="main-container">
+    <ShoppingCartContainer />
+    <OrderSummaryContainer />
+  </main>
+</template>
+
+<script>
+import OrderSummaryContainer from "@/containers/OrderSummaryContainer";
+import ShoppingCartContainer from "@/containers/ShoppingCartContainer";
+
+export default {
+  name: "Home",
+  components: { ShoppingCartContainer, OrderSummaryContainer }
+};
+</script>
---
+++ src/pages/ProductDetail.vue
@@ -0,0 +1,12 @@
+<template>
+    <ProductDetailContainer />
+</template>
+
+<script>
+import ProductDetailContainer from "@/containers/ProductDetailContainer";
+
+export default {
+  name: "Home",
+  components: { ProductDetailContainer }
+};
+</script>
